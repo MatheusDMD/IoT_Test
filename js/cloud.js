6f2c398163bb6568a460ea64ab6bbf7cbe3e8e81
Matheus Marotzke <matheusdmd@al.insper.edu.br>
2016-10-20 11:16:20 -0200

    switch

--- js/cloud.js
+++ js/cloud.js
@@ -13,6 +13,30 @@ var database = firebase.database();
 var texto = $("#meu-texto");
 var liga = $("#liga");
 var desliga = $("#desliga");
+var my_switch = $("#myonoffswitch");
+
+switchs.on("click", function() {
+  if(database.ref("/switch") == 0){
+    database.ref("/switch").set(1);
+    my_switch.checked = true;
+  } else {
+    database.ref("/switch").set(0);
+    my_switch.checked = false;
+  }
+});
+
+function update_switch(){
+  if(database.ref("/switch") == 1){
+    my_switch.checked == true;
+  } else {
+    my_switch.checked == false;
+  }
+}
+
+function startTimer() {
+    setInterval(update_switch, 500);
+}
+
 database.ref("switch").on("value", function(snapshot) {
 	texto.html(snapshot.val());
 });
